667f216b9c33a3da27e518bb277773b436bec3c1
Miguel Grinberg <miguelgrinberg50@gmail.com>
2012-12-31 11:52:43 -0800

    fixed sqlalchemy version at 0.7.9 since sqlalchemy-migrate does not yet work with 0.8

--- setup.py
+++ setup.py
@@ -12,6 +12,7 @@ if sys.platform == 'win32':
     subprocess.call([os.path.join('flask', bin, 'pip'), 'install', '--no-deps', 'lamson', 'chardet', 'flask-mail'])
 else:
     subprocess.call([os.path.join('flask', bin, 'pip'), 'install', 'flask-mail'])
+subprocess.call([os.path.join('flask', bin, 'pip'), 'install', 'sqlalchemy==0.7.9'])
 subprocess.call([os.path.join('flask', bin, 'pip'), 'install', 'flask-sqlalchemy'])
 subprocess.call([os.path.join('flask', bin, 'pip'), 'install', 'sqlalchemy-migrate'])
 subprocess.call([os.path.join('flask', bin, 'pip'), 'install', 'flask-whooshalchemy'])
