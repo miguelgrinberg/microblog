95282d34be4c25f4402feeac9bd92c4b22f7eae0
Miguel Grinberg <miguel.grinberg@gmail.com>
2017-09-11 14:30:21 -0700

    Chapter 4: Database (v0.4)

--- config.py
+++ config.py
@@ -1,4 +1,9 @@
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
--- microblog.py
+++ microblog.py
@@ -1,1 +1,7 @@
-from app import app
+from app import app, db
+from app.models import User, Post
+
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Post': Post}
