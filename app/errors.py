f54844763ef77d41958c72457bf6904864cadf9a
Miguel Grinberg <miguel.grinberg@gmail.com>
2017-09-15 00:03:20 -0700

    Chapter 7: Error Handling (v0.7)

---
+++ app/errors.py
@@ -0,0 +1,13 @@
+from flask import render_template
+from app import app, db
+
+
+@app.errorhandler(404)
+def not_found_error(error):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_error(error):
+    db.session.rollback()
+    return render_template('500.html'), 500
