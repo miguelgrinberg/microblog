5a60dad89bb94dce9455a13776d6d7fdb813f5f4
Miguel Grinberg <miguel.grinberg@gmail.com>
2017-09-17 22:41:40 -0700

    Chapter 9: Pagination (v0.9)

--- app/forms.py
+++ app/forms.py
@@ -55,3 +55,9 @@ class EditProfileForm(FlaskForm):
 
 class EmptyForm(FlaskForm):
     submit = SubmitField('Submit')
+
+
+class PostForm(FlaskForm):
+    post = TextAreaField('Say something', validators=[
+        DataRequired(), Length(min=1, max=140)])
+    submit = SubmitField('Submit')
--- config.py
+++ config.py
@@ -12,3 +12,4 @@ class Config:
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['your-email@example.com']
+    POSTS_PER_PAGE = 25
