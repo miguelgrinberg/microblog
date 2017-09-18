f525e4c270e4f61ba90174fd7dc735d4f8868ba7
Miguel Grinberg <miguel.grinberg@gmail.com>
2017-09-17 22:41:40 -0700

    Chapter 9: Pagination (v0.9)

--- app/forms.py
+++ app/forms.py
@@ -46,3 +46,8 @@ class EditProfileForm(FlaskForm):
             user = User.query.filter_by(username=self.username.data).first()
             if user is not None:
                 raise ValidationError('Please use a different username.')
+
+
+class PostForm(FlaskForm):
+    post = TextAreaField('Say something', validators=[DataRequired()])
+    submit = SubmitField('Submit')
