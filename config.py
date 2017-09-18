8f3ece2e541c5f2d9c779c2122df6c0007595abb
Miguel Grinberg <miguel.grinberg@gmail.com>
2017-09-17 22:41:40 -0700

    Chapter 9: Pagination (v0.9)

--- config.py
+++ config.py
@@ -13,3 +13,4 @@ class Config(object):
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['your-email@example.com']
+    POSTS_PER_PAGE = 25
