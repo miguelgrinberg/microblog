e8c492c0a00b8d07c871f03887ba80262d61736d
Miguel Grinberg <miguel.grinberg@gmail.com>
2017-09-26 00:16:46 -0700

    Chapter 10: Email Support (v0.10)

---
+++ app/email.py
@@ -0,0 +1,27 @@
+from threading import Thread
+from flask import render_template
+from flask_mail import Message
+from app import app, mail
+
+
+def send_async_email(app, msg):
+    with app.app_context():
+        mail.send(msg)
+
+
+def send_email(subject, sender, recipients, text_body, html_body):
+    msg = Message(subject, sender=sender, recipients=recipients)
+    msg.body = text_body
+    msg.html = html_body
+    Thread(target=send_async_email, args=(app, msg)).start()
+
+
+def send_password_reset_email(user):
+    token = user.get_reset_password_token()
+    send_email('[Microblog] Reset Your Password',
+               sender=app.config['ADMINS'][0],
+               recipients=[user.email],
+               text_body=render_template('email/reset_password.txt',
+                                         user=user, token=token),
+               html_body=render_template('email/reset_password.html',
+                                         user=user, token=token))
