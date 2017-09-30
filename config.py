566fa6331c3bce3dd2083961b9c8ded3803312b2
Miguel Grinberg <miguel.grinberg@gmail.com>
2017-09-30 00:21:17 -0700

    Chapter 13: I18n and L10n (v0.13)

--- config.py
+++ config.py
@@ -13,4 +13,5 @@ class Config(object):
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['your-email@example.com']
+    LANGUAGES = ['en', 'es']
     POSTS_PER_PAGE = 25
