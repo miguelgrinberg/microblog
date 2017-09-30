115a7d489881c1d0c162655baa3c20fc8c272e36
Miguel Grinberg <miguel.grinberg@gmail.com>
2017-09-30 00:21:17 -0700

    Chapter 13: I18n and L10n (v0.13)

--- app/email.py
+++ app/email.py
@@ -1,6 +1,7 @@
 from threading import Thread
 from flask import render_template
 from flask_mail import Message
+from flask_babel import _
 from app import app, mail
 
 
@@ -18,7 +19,7 @@ def send_email(subject, sender, recipients, text_body, html_body):
 
 def send_password_reset_email(user):
     token = user.get_reset_password_token()
-    send_email('[Microblog] Reset Your Password',
+    send_email(_('[Microblog] Reset Your Password'),
                sender=app.config['ADMINS'][0],
                recipients=[user.email],
                text_body=render_template('email/reset_password.txt',
