a50ee3f80a032db63111dd5139bce0050b8a5727
Miguel Grinberg <miguel.grinberg@gmail.com>
2017-10-05 15:34:15 -0700

    Chapter 14: Ajax (v0.14)

--- config.py
+++ config.py
@@ -13,4 +13,5 @@ class Config:
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['your-email@example.com']
     LANGUAGES = ['en', 'es']
+    MS_TRANSLATOR_KEY = os.environ.get('MS_TRANSLATOR_KEY')
     POSTS_PER_PAGE = 25
