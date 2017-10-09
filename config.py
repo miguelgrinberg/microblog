5f8cd88ddd93ee6f1e2cc895930e774e293d3813
Miguel Grinberg <miguel.grinberg@gmail.com>
2017-10-09 00:09:22 -0700

    Chapter 15: A Better Application Structure (v0.15)

--- config.py
+++ config.py
@@ -1,5 +1,8 @@
 import os
+from dotenv import load_dotenv
+
 basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
 
 
 class Config(object):
--- microblog.py
+++ microblog.py
@@ -1,5 +1,8 @@
-from app import app, db, cli
+from app import create_app, db, cli
 from app.models import User, Post
+
+app = create_app()
+cli.register(app)
 
 
 @app.shell_context_processor
