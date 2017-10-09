64ed7642b924f6942b2efe9766b36b3f4fe62430
Miguel Grinberg <miguel.grinberg@gmail.com>
2017-10-09 00:09:22 -0700

    Chapter 15: A Better Application Structure (v0.15)

--- app/models.py
+++ app/models.py
@@ -1,10 +1,11 @@
 from datetime import datetime
 from hashlib import md5
 from time import time
+from flask import current_app
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 import jwt
-from app import app, db, login
+from app import db, login
 
 
 followers = db.Table(
@@ -64,12 +65,13 @@ class User(UserMixin, db.Model):
     def get_reset_password_token(self, expires_in=600):
         return jwt.encode(
             {'reset_password': self.id, 'exp': time() + expires_in},
-            app.config['SECRET_KEY'], algorithm='HS256').decode('utf-8')
+            current_app.config['SECRET_KEY'],
+            algorithm='HS256').decode('utf-8')
 
     @staticmethod
     def verify_reset_password_token(token):
         try:
-            id = jwt.decode(token, app.config['SECRET_KEY'],
+            id = jwt.decode(token, current_app.config['SECRET_KEY'],
                             algorithms=['HS256'])['reset_password']
         except:
             return
