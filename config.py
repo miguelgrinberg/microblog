1e44f0bb143936a5587a1bacbb1844ae1bfcbee2
Miguel Grinberg <miguel.grinberg@gmail.com>
2017-10-16 22:24:15 -0700

    Chapter 18: Deployment on Heroku (v0.18)

--- config.py
+++ config.py
@@ -10,6 +10,7 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
     MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
