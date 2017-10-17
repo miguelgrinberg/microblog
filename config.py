f1b2b6f54c4cb63d2e2e100ab71ed7286d0b5404
Miguel Grinberg <miguel.grinberg@gmail.com>
2017-10-16 22:24:15 -0700

    Chapter 18: Deployment on Heroku (v0.18)

--- config.py
+++ config.py
@@ -8,8 +8,10 @@ load_dotenv(os.path.join(basedir, '.env'))
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     SERVER_NAME = os.environ.get('SERVER_NAME')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', '').replace(
+        'postgres://', 'postgresql://') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
+    LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
     MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
