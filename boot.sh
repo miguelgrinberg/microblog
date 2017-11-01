0372a4186e203967cf3a5499d5e46e02d94d9748
Miguel Grinberg <miguel.grinberg@gmail.com>
2017-11-01 13:03:19 -0700

    Chapter 19: Deployment on Docker Containers (v0.19)

---
+++ boot.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# this script is used to boot a Docker container
+source venv/bin/activate
+while true; do
+    flask db upgrade
+    if [[ "$?" == "0" ]]; then
+        break
+    fi
+    echo Deploy command failed, retrying in 5 secs...
+    sleep 5
+done
+flask translate compile
+exec gunicorn -b :5000 --access-logfile - --error-logfile - microblog:app
