a7dac64395c5a6f4e46832e484aafd139b2f729e
Miguel Grinberg <miguel.grinberg@gmail.com>
2017-11-01 23:43:40 -0700

    Chapter 23: Application Programming Interfaces (APIs) (v0.23)

--- app/__init__.py
+++ app/__init__.py
@@ -50,6 +50,9 @@ def create_app(config_class=Config):
     from app.main import bp as main_bp
     app.register_blueprint(main_bp)
 
+    from app.api import bp as api_bp
+    app.register_blueprint(api_bp, url_prefix='/api')
+
     if not app.debug and not app.testing:
         if app.config['MAIL_SERVER']:
             auth = None
