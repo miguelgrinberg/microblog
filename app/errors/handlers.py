872c76e39be9d31e3f78ad035a22e3bdd1ed034a
Miguel Grinberg <miguel.grinberg@gmail.com>
2017-11-01 23:43:40 -0700

    Chapter 23: Application Programming Interfaces (APIs) (v0.23)

--- app/errors/handlers.py
+++ app/errors/handlers.py
@@ -1,14 +1,24 @@
-from flask import render_template
+from flask import render_template, request
 from app import db
 from app.errors import bp
+from app.api.errors import error_response as api_error_response
+
+
+def wants_json_response():
+    return request.accept_mimetypes['application/json'] >= \
+        request.accept_mimetypes['text/html']
 
 
 @bp.app_errorhandler(404)
 def not_found_error(error):
+    if wants_json_response():
+        return api_error_response(404)
     return render_template('errors/404.html'), 404
 
 
 @bp.app_errorhandler(500)
 def internal_error(error):
     db.session.rollback()
+    if wants_json_response():
+        return api_error_response(500)
     return render_template('errors/500.html'), 500
