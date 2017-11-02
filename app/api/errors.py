168da5c050182aa9405286d75f4b1fcfd3ec0936
Miguel Grinberg <miguel.grinberg@gmail.com>
2017-11-01 23:43:40 -0700

    Chapter 23: Application Programming Interfaces (APIs) (v0.23)

---
+++ app/api/errors.py
@@ -0,0 +1,15 @@
+from flask import jsonify
+from werkzeug.http import HTTP_STATUS_CODES
+
+
+def error_response(status_code, message=None):
+    payload = {'error': HTTP_STATUS_CODES.get(status_code, 'Unknown error')}
+    if message:
+        payload['message'] = message
+    response = jsonify(payload)
+    response.status_code = status_code
+    return response
+
+
+def bad_request(message):
+    return error_response(400, message)
