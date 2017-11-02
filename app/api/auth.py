02aae8d9816fb23b72cbeca2ac945ac2d48a6ed0
Miguel Grinberg <miguel.grinberg@gmail.com>
2017-11-01 23:43:40 -0700

    Chapter 23: Application Programming Interfaces (APIs) (v0.23)

---
+++ app/api/auth.py
@@ -0,0 +1,28 @@
+from flask_httpauth import HTTPBasicAuth, HTTPTokenAuth
+from app.models import User
+from app.api.errors import error_response
+
+basic_auth = HTTPBasicAuth()
+token_auth = HTTPTokenAuth()
+
+
+@basic_auth.verify_password
+def verify_password(username, password):
+    user = User.query.filter_by(username=username).first()
+    if user and user.check_password(password):
+        return user
+
+
+@basic_auth.error_handler
+def basic_auth_error(status):
+    return error_response(status)
+
+
+@token_auth.verify_token
+def verify_token(token):
+    return User.check_token(token) if token else None
+
+
+@token_auth.error_handler
+def token_auth_error(status):
+    return error_response(status)
