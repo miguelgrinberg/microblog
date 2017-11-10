f981b1c67664e3c85baca36d62e87172b09fad3f
Miguel Grinberg <miguel.grinberg@gmail.com>
2017-11-09 19:20:27 -0800

    Chapter 20: Some JavaScript Magic (v0.20)

--- app/main/routes.py
+++ app/main/routes.py
@@ -77,6 +77,13 @@ def user(username):
                            next_url=next_url, prev_url=prev_url)
 
 
+@bp.route('/user/<username>/popup')
+@login_required
+def user_popup(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    return render_template('user_popup.html', user=user)
+
+
 @bp.route('/edit_profile', methods=['GET', 'POST'])
 @login_required
 def edit_profile():
