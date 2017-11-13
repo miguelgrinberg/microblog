88d52181dc0f4960a4dfe9cc9e323631a34e7408
Miguel Grinberg <miguel.grinberg@gmail.com>
2017-11-12 23:53:18 -0800

    Chapter 21: User Notifications (v0.21)

--- microblog.py
+++ microblog.py
@@ -1,11 +1,12 @@
 import sqlalchemy as sa
 import sqlalchemy.orm as so
 from app import create_app, db
-from app.models import User, Post
+from app.models import User, Post, Message, Notification
 
 app = create_app()
 
 
 @app.shell_context_processor
 def make_shell_context():
-    return {'sa': sa, 'so': so, 'db': db, 'User': User, 'Post': Post}
+    return {'sa': sa, 'so': so, 'db': db, 'User': User, 'Post': Post,
+            'Message': Message, 'Notification': Notification}
