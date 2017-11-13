acd13aff1b193ab5efd767a04395a30c2f4ba438
Miguel Grinberg <miguel.grinberg@gmail.com>
2017-11-12 23:53:18 -0800

    Chapter 21: User Notifications (v0.21)

--- microblog.py
+++ microblog.py
@@ -1,5 +1,5 @@
 from app import create_app, db, cli
-from app.models import User, Post
+from app.models import User, Post, Message, Notification
 
 app = create_app()
 cli.register(app)
@@ -7,4 +7,5 @@ cli.register(app)
 
 @app.shell_context_processor
 def make_shell_context():
-    return {'db': db, 'User': User, 'Post': Post}
+    return {'db': db, 'User': User, 'Post': Post, 'Message': Message,
+            'Notification': Notification}
