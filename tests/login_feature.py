852e2c9356d8d82499590bc7bc06f2f8f221615b
Aleksa V <viganjg12@gmail.com>
2023-03-03 18:00:48 -0500

    Initial setup for testing

---
+++ tests/login_feature.py
@@ -0,0 +1,8 @@
+import unittest
+
+class LoginFeatureTests(unittest.TestCase):
+        def test_working(self):
+                self.assertTrue(True)
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
